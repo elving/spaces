6d310c84786d4acae6496a4757d618feb4f3f909
Elving Rodriguez <elving.pr@gmail.com>
2016-08-14 21:11:11 -0400

    Fix deploy-static script.

--- tasks/deploy-static.sh
+++ tasks/deploy-static.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync ./build/public s3://next.joinspaces.co/static/ --acl public-read --profile spaces
+aws s3 sync ./build/public s3://assets.joinspaces.co/static/ --acl public-read --profile spaces

7be89a9ffe4b70da73d6e086d616b8f7e415a79d
Elving Rodriguez <elving.pr@gmail.com>
2016-08-17 00:58:39 -0400

    Fix card tags autoscroll.

--- app/components/card/CardTags.jsx
+++ app/components/card/CardTags.jsx
@@ -52,11 +52,11 @@ export default class CardTags extends Component {
 
     this.setState({
       isHovering: false
-    }, () => {
-      if (props.autoScroll) {
-        this.setInterval()
-      }
     })
+
+    if (props.autoScroll) {
+      this.setInterval()
+    }
   }
 
   setInterval = () => {

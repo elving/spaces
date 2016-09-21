cf31c8dc99a7f50dfc6047c4019d2778092e7f3a
Elving Rodriguez <elving.pr@gmail.com>
2016-09-21 01:40:02 -0400

    Fix suggestions

--- app/api/user/getSuggestions.js
+++ app/api/user/getSuggestions.js
@@ -1,11 +1,16 @@
 import get from 'lodash/get'
 import set from 'lodash/set'
 import map from 'lodash/map'
+import filter from 'lodash/filter'
 import concat from 'lodash/concat'
+import flatten from 'lodash/flatten'
 import isEmpty from 'lodash/isEmpty'
+import includes from 'lodash/includes'
 import mongoose from 'mongoose'
 
 import parseError from '../utils/parseError'
+import toStringId from '../utils/toStringId'
+import toIdsFromPath from '../utils/toIdsFromPath'
 import { default as searchProducts } from '../product/search'
 
 const getSpaces = createdBy => (
@@ -51,7 +56,12 @@ export default user => (
             const products = get(productsSearch, 'results', [])
 
             if (!isEmpty(products)) {
-              set(space, 'products', products)
+              set(space, 'products', filter(products, product =>
+                !includes(
+                  flatten(toIdsFromPath(space, 'products')),
+                  toStringId(product)
+                )
+              ))
 
               spacesWithSuggestions = concat(
                 [], spacesWithSuggestions, space
